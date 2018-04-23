043341cb2a6390dc4bd1e55b6875b8408f527a61
Abdul Shaikh <abdulamite@gmail.com>
2018-04-23 15:31:59 -0700

    changed names of folder, and add tab effect

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ const options = document.querySelectorAll('select');
 const submit = document.querySelector('#submit');
 let attempts = 0;
 
+let answer_set = ["Red","Orange","Yellow","Green","Blue","Purple"];
+
 options.forEach(option =>{
 
+  //The purpose of this function is to enable the submit button at the bottom of the screen.  
   function enableSubmit(){
 
       // Grab selected value
@@ -12,7 +15,7 @@ options.forEach(option =>{
       // get parent container node
       const box = this.parentElement;
       // set path to lizard images
-      const img = `../acessible_lizards/imgs/${color}.png`;
+      const img = `../accessible_lizards/imgs/${color}.png`;
       // Set the current img to the new lizard image
       box.querySelector('IMG').src = img;
 
@@ -36,6 +39,36 @@ options.forEach(option =>{
 });
 
 
+// Switch between reflected and absorbed answer set ans tab styles
+const reflected = document.querySelector('#reflected');
+const absorbed = document.querySelector('#absorbed');
+
+reflected.addEventListener('click',() =>{
+  console.log('Reflected Answer Set');
+  absorbed.style.transform = "translateY(0px)";
+  absorbed.style.backgroundColor = "#666666";
+  absorbed.style.color = "white";
+
+  reflected.style.transform = "translateY(2px)";
+  reflected.style.backgroundColor = "white";
+  reflected.style.color = "#666666";
+  return answer_set = ["Red","Orange","Yellow","Green","Blue","Purple"];
+});
+
+absorbed.addEventListener('click',() =>{
+  console.log('Absorbed Answer Set');
+  reflected.style.transform = "translateY(0px)";
+  reflected.style.backgroundColor = "#666666";
+  reflected.style.color = "white";
+
+  absorbed.style.transform = "translateY(2px)";
+  absorbed.style.backgroundColor = "white";
+  absorbed.style.color = "#666666";
+  return answer_set = ["Purple","Blue","Green","Yellow","Orange","Red"];
+});
+
+console.log(answer_set);
+
 // check users answers
 function checkAnswer(){
   const answers = []
@@ -45,28 +78,27 @@ function checkAnswer(){
 
   points = 0;
   // Check for correct answers
-  if(answers[0] == "Red"){
+  if(answers[0] == answer_set[0]){
     points++;
   }
-  if(answers[1] == "Orange"){
+  if(answers[1] == answer_set[1]){
     points++;
   }
-  if(answers[2] == "Yellow"){
+  if(answers[2] == answer_set[2]){
     points++;
   }
-  if(answers[3] == "Green"){
+  if(answers[3] == answer_set[3]){
     points++;
   }
-  if(answers[4] == "Blue"){
+  if(answers[4] == answer_set[4]){
     points++;
   }
-  if(answers[5] == "Purple"){
+  if(answers[5] == answer_set[5]){
     points++;
   }
 
   let feedbackText = `You have scored ${points} points`
   const feedback = document.querySelector('.feedback');
-
 
   if(feedback){
     document.querySelector('.feedback').firstChild.remove();
@@ -87,7 +119,7 @@ function checkAnswer(){
       feedbackText = `You are ${((points/6)*100).toFixed(2)}% correct. Make sure you are not repeating the same color.`;
     }
     else if (attempts >= 3) {
-      feedbackText = `You are ${((points/6)*100).toFixed(2)}% correct. More than 2 attempts`;
+      feedbackText = `You are ${((points/6)*100).toFixed(2)}% correct. Try again.`;
     }
     // feedbackText = `You are ${((points/6)*100).toFixed(2)}% correct. Keep trying! This is attempt #${attempts}`;
     feedback.style.backgroundColor = '#e27e7e';
